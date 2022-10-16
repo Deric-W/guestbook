f82ff905f9c0a1e602fcfe252254e3d979f1ab7f
Marco <marco.unger@mailbox.tu-dresden.de>
2022-10-16 16:06:49 +0200

    HTML Page - added editing form for email + label (under the name editing field) - added text (email) under the name in posting section
    + changed login/logout hyperlinks to buttons
    
    GuestbookEntry
    - added email as attribute of Entry
    - added email argument to constructor
    
    messages.properties
    - added email (guestbook.form.email)
    
    Tests
    - adopt all tests to create entries with 3 arguements (name, email and text)

--- src/main/java/guestbook/Application.java
+++ src/main/java/guestbook/Application.java
@@ -60,11 +60,11 @@ public class Application {
 		return args -> {
 
 			Stream.of( //
-					new GuestbookEntry("H4xx0r", "first!!!"), //
-					new GuestbookEntry("Arni", "Hasta la vista, baby"), //
-					new GuestbookEntry("Duke Nukem",
+					new GuestbookEntry("H4xx0r", "Hax@louds.com", "first!!!"), //
+					new GuestbookEntry("Arni", "arnold.barth@hotmail.de", "Hasta la vista, baby"), //
+					new GuestbookEntry("Duke Nukem", "lux.haram@web.de",
 							"It's time to kick ass and chew bubble gum. And I'm all out of gum."), //
-					new GuestbookEntry("Gump1337",
+					new GuestbookEntry("Gump1337", "gulp@t-online.de",
 							"Mama always said life was like a box of chocolates. You never know what you're gonna get.")) //
 					.forEach(guestbook::save);
 		};
--- src/main/java/guestbook/GuestbookEntry.java
+++ src/main/java/guestbook/GuestbookEntry.java
@@ -34,7 +34,7 @@ import org.springframework.util.Assert;
 class GuestbookEntry {
 
 	private @Id @GeneratedValue Long id;
-	private final String name, text;
+	private final String name, email, text;
 	private final LocalDateTime date;
 
 	/**
@@ -43,12 +43,14 @@ class GuestbookEntry {
 	 * @param name must not be {@literal null} or empty
 	 * @param text must not be {@literal null} or empty
 	 */
-	public GuestbookEntry(String name, String text) {
+	public GuestbookEntry(String name, String email, String text) {
 
 		Assert.hasText(name, "Name must not be null or empty!");
+        Assert.hasText(email, "Email must not be null or empty!");
 		Assert.hasText(text, "Text must not be null or empty!");
 
 		this.name = name;
+        this.email = email;
 		this.text = text;
 		this.date = LocalDateTime.now();
 	}
@@ -56,11 +58,12 @@ class GuestbookEntry {
 	@SuppressWarnings("unused")
 	private GuestbookEntry() {
 		this.name = null;
+        this.email = null;
 		this.text = null;
 		this.date = null;
 	}
 
-	public String getName() {
+    public String getName() {
 		return name;
 	}
 
@@ -75,4 +78,8 @@ class GuestbookEntry {
 	public String getText() {
 		return text;
 	}
+
+    public String getEmail(){
+        return email;
+    }
 }
